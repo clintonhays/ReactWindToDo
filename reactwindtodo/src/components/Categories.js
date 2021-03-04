7faf093c09aeadf40d73f39a3d6bde7be4a4cb7b
clintonhays <clintonhays84@gmail.com>
2021-03-04 16:33:15 +0900

    add todo text input and urgency capture functions

--- reactwindtodo/src/components/Categories.js
+++ reactwindtodo/src/components/Categories.js
@@ -1,21 +1,31 @@
-import React from "react";
+import { useState } from "react";
 
 const Categories = () => {
+	const [urgency, setUrgency] = useState("");
+
 	return (
 		<div className="text-center">
 			<label htmlFor="categories">
 				Urgency:
 				<br />
-				<select name="categories" id="categories">
+				<select
+					name="categories"
+					id="categories"
+					onChange={(e) => {
+						const urgencyLevel = e.target.value;
+						setUrgency(urgencyLevel);
+					}}
+				>
 					<option value="" hidden>
 						Level of Urgency
 					</option>
-					<option value="urgent">Critical</option>
+					<option value="critical">Critical</option>
 					<option value="high">High</option>
 					<option value="medium">Medium</option>
 					<option value="low">Low</option>
 				</select>
 			</label>
+			{urgency}
 		</div>
 	);
 };
