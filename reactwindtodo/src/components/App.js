9ba854b4993238e1a6be74d93e24df36be624a95
clintonhays <clintonhays84@gmail.com>
2021-03-04 16:34:34 +0900

    add todo text input and urgency capture functions

--- reactwindtodo/src/components/App.js
+++ reactwindtodo/src/components/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { uuid } from "uuidv4";
 import "../style/App.css";
 import Categories from "./Categories";
 import TaskCount from "./TaskCount";
@@ -7,6 +8,21 @@ import TodoInput from "./TodoInput";
 function App() {
 	const [todos, setTodos] = useState([]);
 
+	const newTodo = (text, category) => {
+		console.log(text);
+		if (text === "") {
+			// inputError()
+			return;
+		}
+		if (category === "") {
+			// categoryError()
+			return;
+		}
+		setTodos((prevState) => {
+			return [...prevState, { id: uuid(), text: text, category: category }];
+		});
+	};
+
 	return (
 		<>
 			<div className="bg-green-200">
@@ -14,10 +30,10 @@ function App() {
 			</div>
 			<div className="info border-2 border-red-600 flex justify-around">
 				<TaskCount />
-				<Categories />
+				<Categories newTodo={newTodo} />
 			</div>
 			<div className="todoInput">
-				<TodoInput />
+				<TodoInput newTodo={newTodo} />
 			</div>
 		</>
 	);
--- reactwindtodo/src/components/TodoInput.js
+++ reactwindtodo/src/components/TodoInput.js
@@ -1,17 +1,30 @@
-import React from "react";
+import { useRef } from "react";
 
-const TodoInput = () => {
+const TodoInput = (props) => {
+	const todoInput = useRef("");
+
+	const newTodo = (e) => {
+		props.newTodo(todoInput.current.value);
+		todoInput.current.value = "";
+		todoInput.current.focus();
+		e.preventDefault();
+	};
+
 	return (
 		<div className="text-center">
-			<label htmlFor="todoInput">Add Task:</label>
-			<br />
-			<input
-				className="w-1/5 border-2 border-red-600"
-				type="text"
-				name="todoInput"
-				id="todoInput"
-				placeholder="What would you like...todo?"
-			/>
+			<form onSubmit={newTodo}>
+				<label htmlFor="todoInput">Add Task:</label>
+				<br />
+				<input
+					className="w-1/5 border-2 border-red-600"
+					type="text"
+					name="todoInput"
+					id="todoInput"
+					placeholder="What would you like...todo?"
+					ref={todoInput}
+				/>
+				<button>Add Task</button>
+			</form>
 		</div>
 	);
 };
